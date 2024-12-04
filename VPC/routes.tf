6f61dffd7c1576228ff705cc584c454e8e4182b5
khwahishindoria <khwahishindoria@gmail.com>
2024-12-04 15:18:07 +0530

    Create routes.tf

---
+++ VPC/routes.tf
@@ -0,0 +1,36 @@
+resource "aws_route_table" "public-rt" {
+    vpc_id = aws_vpc.prod-vpc.id
+    tags = {
+      Name = "public-rt"
+    }
+
+        route {
+            cidr_block = "0.0.0.0/0"
+            gateway_id = aws_internet_gateway.prod-igw.id
+        }
+
+}
+
+resource "aws_route_table_association" "prod-vpc-RT-ASC" {
+    subnet_id = aws_subnet.prod-vpc_subnet1.id
+    route_table_id = aws_route_table.public-rt.id
+  
+}
+
+resource "aws_route_table" "private-rt" {
+    vpc_id = aws_vpc.prod-vpc.id
+    tags = {
+      Name = "private-rt"
+    }
+
+       # route {
+       #     cidr_block = "0.0.0.0/0"
+       # }
+
+}
+
+resource "aws_route_table_association" "prod-vpc-RT-ASC-private" {
+    subnet_id = aws_subnet.prod-vpc_subnet2.id
+    route_table_id = aws_route_table.private-rt.id
+  
+}
