6a471013d73a5de72c1ee3b658a67921eeabdfa6
khwahishindoria <khwahishindoria@gmail.com>
2024-12-04 20:18:37 +0530

    Update main.tf

--- VPC/VPC-Peering/main.tf
+++ VPC/VPC-Peering/main.tf
@@ -8,15 +8,16 @@ variable "prod-vpc_cidr-1" {
 variable "prod-vpc_cidr-2" {
     default = "172.16.0.0/16"
 }
-resource "aws_vpc" "prod-vpc" {
+resource "aws_vpc" "prod-vpc-1" {
     tags = {
         Name = "PROD-VPC-1"
     }
-    cidr_block = var.prod-vpc_cidr 
+    cidr_block = var.prod-vpc_cidr-1
 }
-resource "aws_vpc" "prod-vpc" {
+resource "aws_vpc" "prod-vpc-2" {
     tags = {
         Name = "PROD-VPC-2"
     }
     cidr_block = var.prod-vpc_cidr-2
 }
+
