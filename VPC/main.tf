180ecc8e04f3b2f687a8f06492f50d381635cb45
khwahishindoria <khwahishindoria@gmail.com>
2024-12-04 15:16:33 +0530

    Create main.tf

---
+++ VPC/main.tf
@@ -0,0 +1,33 @@
+provider "aws" {
+    region = "us-east-1" 
+}
+
+
+## VPC Creation  ## 
+variable "prod-vpc_cidr" {
+    default = "10.0.0.0/16"
+}
+
+resource "aws_vpc" "prod-vpc" {
+    tags = {
+        Name = "PROD-VPC"
+    }
+    cidr_block = var.prod-vpc_cidr 
+}
+
+resource "aws_internet_gateway" "prod-igw" {
+    vpc_id = aws_vpc.prod-vpc.id
+  
+}
+
+resource "aws_eip" "eip" {
+  domain = "vpc"
+  tags = {
+    Name = "PROD-ELASTIC_IP"
+  }
+}
+
+resource "aws_eip_association" "eip_assoc" {
+  instance_id   = aws_instance.Web-Server.id
+  allocation_id = aws_eip.eip.id
+}
