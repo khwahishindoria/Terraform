e6eadef0df47d0641a854bc60ff7c760465eef9f
khwahishindoria <khwahishindoria@gmail.com>
2024-12-29 21:36:17 +0530

    Update ec2.tf

--- kubeadm-with-ec2/ec2.tf
+++ kubeadm-with-ec2/ec2.tf
@@ -3,7 +3,7 @@ resource "aws_key_pair" "prod-keypair" {
     public_key = file("~/.ssh/id_rsa.pub")
 }
 
-resource "aws_instance" "master-01" {
+resource "aws_instance" "Bastion" {
   ami                     = "ami-0866a3c8686eaeeba"
   instance_type           = "t2.micro"
   subnet_id = aws_subnet.prod-vpc_subnet1.id
@@ -11,7 +11,7 @@ resource "aws_instance" "master-01" {
   
   vpc_security_group_ids = [ aws_security_group.prod-vpc-SG.id ]
   tags = {
-    Name = "master-01"
+    Name = "Bastion"
   }
 
   connection {
@@ -34,15 +34,37 @@ resource "aws_instance" "master-01" {
     depends_on = [ aws_vpc.prod-vpc, aws_route_table.public-rt ]
 }
 
+resource "aws_instance" "master-01" {
+  ami                     = "ami-0866a3c8686eaeeba"
+  instance_type           = "t2.medium"
+  subnet_id = aws_subnet.prod-vpc_subnet2.id
+  key_name = aws_key_pair.prod-keypair.key_name
+  vpc_security_group_ids = [ aws_security_group.prod-vpc-SG.id ]
+
+  tags = {
+    Name = "master-01"
+  }
+  user_data = <<EOF
+    #!/bin/bash
+    sudo apt-get install gnupg
+    sudo apt-get install -y python3-pip
+    sudo apt-get install -y nginx
+
+
+  EOF
+  depends_on = [ aws_vpc.prod-vpc, aws_route_table.public-rt ]
+
+}
+
 resource "aws_instance" "worker-01" {
   ami                     = "ami-0866a3c8686eaeeba"
   instance_type           = "t2.micro"
   subnet_id = aws_subnet.prod-vpc_subnet2.id
   key_name = aws_key_pair.prod-keypair.key_name
   vpc_security_group_ids = [ aws_security_group.prod-vpc-SG.id ]
-
+  for_each = var.ec2-instance-names
   tags = {
-    Name = "worker-01"
+    Name = each.value
   }
   user_data = <<EOF
     #!/bin/bash
