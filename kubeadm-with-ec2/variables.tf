42a2070b4b03ba000acd427b13fce300de571280
khwahishindoria <khwahishindoria@gmail.com>
2025-01-04 22:37:33 +0530

    Update variables.tf

--- kubeadm-with-ec2/variables.tf
+++ kubeadm-with-ec2/variables.tf
@@ -2,3 +2,19 @@ variable "ec2-instance-names" {
   type = set(string)
   default = [ "worker-01", "worker-02" ]
 }
+
+
+output "master-public-ip" {
+  value = aws_instance.master-node.public_ip
+}
+
+output "master-01-privateip" {
+  value = aws_instance.master-node.private_ip
+}
+
+output "worker-nodes-private-ip" {
+
+  value = [ for i in aws_instance.worker-nodes: i.private_ip ]
+}
+
+
