7984787f1687a4228bfbf4dec9d28103f8d7779b
khwahishindoria <khwahishindoria@gmail.com>
2025-01-17 23:03:09 +0530

    hotfix 1 patch deployed

--- kubeadm-with-ec2/alb.tf
+++ kubeadm-with-ec2/alb.tf
@@ -30,7 +30,7 @@ resource "aws_lb_listener" "jenkins_listener" {
 
   default_action {
     type = "forward"
-    target_group_arn = aws_lb_target_group.tg-master.arn
+    target_group_arn = aws_lb_target_group.tg-jenkins.arn
 
     }
 }
