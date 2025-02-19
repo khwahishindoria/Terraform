a65255d998e8651fa84040745e4d288deea1cb7e
khwahishindoria <khwahishindoria@gmail.com>
2025-02-19 23:16:42 +0530

    Update targetgroup.tf

--- kubeadm-with-ec2/targetgroup.tf
+++ kubeadm-with-ec2/targetgroup.tf
@@ -4,28 +4,12 @@ resource "aws_lb_target_group" "tg-master" {
   port        = 80
   protocol    = "HTTP"
   vpc_id      = aws_vpc.prod-vpc.id
+  depends_on = [ aws_lb.prod-alb ]
 }
 
 resource "aws_lb_target_group_attachment" "test" {
   target_group_arn = aws_lb_target_group.tg-master.arn
   target_id        = aws_instance.master-node.id
   port             = 31599
-}
-
-resource "aws_lb_target_group" "tg-jenkins" {
-  name        = "jenkins-node-tg"
-  target_type = "instance"
-  port        = 8080
-  protocol    = "HTTP"
-  vpc_id      = aws_vpc.prod-vpc.id 
-  health_check {
-    path = "/login"
-  }
-}
-
-resource "aws_lb_target_group_attachment" "jenkins-tg-attach" {
-  target_group_arn = aws_lb_target_group.tg-jenkins.arn
-  target_id        = aws_instance.master-node.id
-  port             = 8080
-
+  depends_on = [ aws_lb_target_group.tg-master ]
 }
